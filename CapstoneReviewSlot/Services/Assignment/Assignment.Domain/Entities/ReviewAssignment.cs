beaa2aceff45ef8b06cf6f5b35a9c9155f2dcbe8
But <hoahuynh882003@gmail.com>
2026-04-17 01:42:06 +0700

    [Feat] Add Assignment service with ReviewAssignment and ReviewAssignmentReviewer code-first setup

---
+++ CapstoneReviewSlot/Services/Assignment/Assignment.Domain/Entities/ReviewAssignment.cs
@@ -0,0 +1,21 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Domain.Entities
+{
+    public class ReviewAssignment : BaseEntity
+    {
+        public Guid CapstoneGroupId { get; set; }
+        public Guid ReviewSlotId { get; set; }
+        public string Status { get; set; } = default!;
+        public int ReviewOrder { get; set; }
+        public Guid AssignedBy { get; set; }
+        public DateTime AssignedAt { get; set; }
+
+        public ICollection<ReviewAssignmentReviewer> Reviewers { get; set; } = new List<ReviewAssignmentReviewer>();
+    }
+}
---
+++ CapstoneReviewSlot/Services/Assignment/Assignment.Domain/Entities/ReviewAssignmentReviewer.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Domain.Entities
+{
+    public class ReviewAssignmentReviewer : BaseEntity
+    {
+        public Guid LecturerId { get; set; }
+        public Guid ReviewAssignmentId { get; set; }
+        public string Role { get; set; } = default!;
+
+        public ReviewAssignment ReviewAssignment { get; set; } = default!;
+    }
+}
